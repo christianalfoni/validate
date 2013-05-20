39e7d092465bad93cd183884c2cd37061a4e0d39
christianalfoni <christian.jorgensen@marcello.no>
2013-05-20 13:15:07 +0200

    Updated validate

--- dist/validate.js
+++ dist/validate.js
@@ -20,6 +20,7 @@
     'use strict';
     var
     // Default errorMessages. Can be changed with validator.config({ errorMessages: { required: 'myText' } })
+        missingData = 'You are missing some data',
         errorMessages = {
             required: 'You have to type something!',
             before: 'Date has to be before %',
@@ -40,16 +41,26 @@
             validators = {
             // The value must have content
             required: function (value, data) {
+                if (typeof value === 'undefined') {
+                    return missingData;
+                }
                 return value === undefined || value === null || (typeof value === 'string' && value.length === 0) ? errorMessages.required : true;
             },
             number: function (value, data) {
+                if (typeof value === 'undefined') {
+                    return missingData;
+                }
                 return typeof value === 'number' ? true : errorMessages.number.replace('%', value.toString());
             },
             // Checks if date is before date on an other property in the dataset
             before: function (date, data, prop) {
-                console.log(prop);
                 var beforeDate = date,
                     afterDate = data[prop];
+
+                if (typeof afterDate === 'undefined' || typeof beforeDate === 'undefined') {
+                    return missingData;
+                }
+
                 if (!(date instanceof Date)) {
                     beforeDate = new Date(date);
                 }
@@ -62,6 +73,11 @@
             after: function (date, data, prop) {
                 var beforeDate = data[prop],
                     afterDate = date;
+
+                if (typeof afterDate === 'undefined' || typeof beforeDate === 'undefined') {
+                    return missingData;
+                }
+
                 if (!(date instanceof Date)) {
                     afterDate = new Date(date);
                 }
@@ -72,22 +88,34 @@
             },
             // Value equals length
             length: function (value, data, length) {
+                if (typeof value === 'undefined' || typeof length === 'undefined') {
+                    return missingData;
+                }
                 var length = Number(length);
-                return value.length === length ? true : errorMessages['length'].replace('%', length.toString());
+                return value.toString().length === length ? true : errorMessages['length'].replace('%', length.toString());
             },
             // Value longer than length
             longerThan: function (value, data, length) {
+                if (typeof value === 'undefined' || typeof length === 'undefined') {
+                    return missingData;
+                }
                 var length = Number(length);
                 return value.length > length ? true : errorMessages.longerThan.replace('%', length.toString());
             },
             // Value shorter than length
             shorterThan: function (value, data, length) {
+                if (typeof value === 'undefined' || typeof length === 'undefined') {
+                    return missingData;
+                }
                 var length = Number(length);
-                return value.length < length ? true : errorMessages.longerThan.replace('%', length.toString());
+                return value.length < length ? true : errorMessages.shorterThan.replace('%', length.toString());
             },
             // Valid email
             email: function (value, data) {
-                return value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? true : errorMessages.email;
+                if (typeof value === 'undefined') {
+                    return missingData;
+                }
+                return value.toString().match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ? true : errorMessages.email;
             }
         },
         extended = {},
